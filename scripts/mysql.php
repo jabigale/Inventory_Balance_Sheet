3720a38c632a38d85f0b7623b26d97a4379903ac
Jordan Matthews <jmatthews.programmer@gmail.com>
2023-05-04 18:44:36 -0600

    Add files via upload

---
+++ scripts/mysql.php
@@ -0,0 +1,7 @@
+<?php
+//mysql info
+$dbserver = 'localhost';
+$dbname = 'realetp3_twch';
+$dbuser = 'realetp3_twch';
+$dbpassword = 'fAaFQzFgV2Uy3sB';
+$pdocxn = new PDO('mysql:host=localhost;dbname='.$dbname.'',''.$dbuser.'',''.$dbpassword.'');
